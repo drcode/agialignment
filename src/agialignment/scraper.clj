33ed12934ad2add8c80cb4ac58f87623fafbbfef
Your Name <you@example.com>
2022-08-22 14:46:15 -0500

    wip

--- src/agialignment/scraper.clj
+++ src/agialignment/scraper.clj
@@ -15,7 +15,8 @@
     (io/copy in out)))
 
 (defn fetch-avatar [username]
-  (ea/go (driver) (str "https://twitter.com/" username))
-  (ea/wait-exists (driver) {:css "a[href$='/photo'] img[src]"})
-  (let [url (ea/get-element-attr (driver) {:css "a[href$='/photo'] img[src]"} :src)]
-    (copy url (str "resources/public/avatars/" username))))
+  #d :heeer
+  #d (ea/go (driver) #d (str "https://twitter.com/" username))
+  #d (ea/wait-exists (driver) {:css "a[href$='/photo'] img[src]"})
+  (let [url #d (ea/get-element-attr (driver) {:css "a[href$='/photo'] img[src]"} :src)]
+    #d (copy url (str "resources/public/avatars/" username))))
